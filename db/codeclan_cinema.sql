71dd9de85933a298da3f9bcdea72a74c9525d63e
Alex Bazlinton <abazlinton@gmail.com>
2016-10-15 19:07:03 +0100

    new tables done (hopefully)

--- db/codeclan_cinema.sql
+++ db/codeclan_cinema.sql
@@ -1,24 +1,49 @@
 DROP TABLE IF EXISTS tickets;
 DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS showings;
+DROP TABLE IF EXISTS pricings;
 DROP TABLE IF EXISTS films;
+
+
+CREATE TYPE person_types AS ENUM ('adult','child', 'teen', 'student', 'senior');
+CREATE TYPE film_types AS ENUM ('premium','standard');
 
 
 CREATE TABLE customers(
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255),
   --- Better to specify 
-  funds NUMERIC(20,2)
+  funds NUMERIC(20,2),
+  person_type person_types
 );
 
 CREATE TABLE films(
   id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
+  film_type film_types,
+  release_date date 
+);
+
+
+CREATE TABLE pricings(
+  id SERIAL4 PRIMARY KEY,
+  film_type film_types,
+  person_type person_types,
   price NUMERIC(5,2)
 );
 
+CREATE TABLE showings(
+id SERIAL4 PRIMARY KEY,
+showing_time NUMERIC(6,2),
+film_id INT4 REFERENCES films(id)
+);
 
 CREATE TABLE tickets(
   id SERIAL4 PRIMARY KEY,
-  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+  customer_id INT4 REFERENCES customers(id),
+  showing_id INT4 REFERENCES showings(id),
+  price_id INT4 REFERENCES pricings(id),
+  price_paid INT4
 );
+
+
